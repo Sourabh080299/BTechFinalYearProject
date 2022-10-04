d7b242fcb2a097c7df533c3aebadc0df6be07e18
Sourabh080299 <psourabh080299@gmail.com>
2022-10-04 23:07:54 +0530

    first commit

---
+++ algo/Knn.py
@@ -0,0 +1,6 @@
+from sklearn.neighbors import KNeighborsClassifier
+model_name = ‘K-Nearest Neighbor Classifier’
+knnClassifier = KNeighborsClassifier(n_neighbors = 5, metric = ‘minkowski’, p=2)
+knn_model = Pipeline(steps=[(‘preprocessor’, preprocessorForFeatures), (‘classifier’ , knnClassifier)])
+knn_model.fit(X_train, y_train)
+y_pred = knn_model.predict(X_test)
